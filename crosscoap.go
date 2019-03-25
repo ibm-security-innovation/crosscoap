e1e098e486bbc3036f3f54d88599916f13869210
Justin Collier <justincollier217@gmail.com>
2019-03-25 17:50:49 -0500

    corrects example library implementation

--- crosscoap.go
+++ crosscoap.go
@@ -9,11 +9,15 @@
 //
 //     import (
 //             "log"
+//	           "net"
+//             "os"
+//             "time"
 //
 //             "github.com/ibm-security-innovation/crosscoap"
 //     )
 //
 //     func main() {
+//             timeout := time.Duration(10 * time.Second)
 //             appLog := log.New(os.Stderr, "[example] ", log.LstdFlags)
 //             udpAddr, err := net.ResolveUDPAddr("udp", "0.0.0.0:5683")
 //             if err != nil {
@@ -24,10 +28,10 @@
 //                     errorLog.Fatalln("Can't listen on UDP")
 //             }
 //             defer udpListener.Close()
-//             p := crosscoap.COAPProxy{
+//             p := crosscoap.Proxy{
 //                     Listener:   udpListener,
 //                     BackendURL: "http://127.0.0.1:8000/",
-//                     Timeout:    10 * time.Second,
+//                     Timeout:    &timeout,
 //                     AccessLog:  appLog,
 //                     ErrorLog:   appLog,
 //             }
